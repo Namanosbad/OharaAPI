e350322440b67d5540dfd216621b3747a12eefa9
Matheus <151960068+Namanosbad@users.noreply.github.com>
2026-02-26 17:18:12 -0300

    Refactor LivroService.

--- Ohara.API.Database/Repositories/LivroRepository.cs
+++ Ohara.API.Database/Repositories/LivroRepository.cs
@@ -1,32 +1,51 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Ohara.API.Domain.Entities;
 using Ohara.API.Domain.Interfaces;
+using Ohara.API.Shared.Enums;
 
 namespace Ohara.API.Database.Repositories
 {
     public class LivroRepository : ILivroRepository
     {
         private readonly OharaDbContext _dbContext;
+
         public LivroRepository(OharaDbContext oharaDbContext)
         {
             _dbContext = oharaDbContext;
         }
 
-        public async Task<IEnumerable<Livro>> BuscarPorTituloAsync(string titulo)
+        public async Task<Livro?> ObterPorIdComAutorAsync(Guid id)
         {
             return await _dbContext.Livros
-                 .Where(i => i.Titulo.Contains(titulo))
-                 .ToListAsync();
+                .AsNoTracking()
+                .Include(l => l.Autor)
+                .FirstOrDefaultAsync(l => l.Id == id);
         }
 
-        public async Task<IEnumerable<Livro>> LivroPorGenero(string genero)
+        public async Task<IEnumerable<Livro>> ObterTodosComAutorAsync()
         {
-
             return await _dbContext.Livros
-                    .AsNoTracking()
-                    .Where(i => i.Genero.ToString().Contains(genero))
-                    .ToListAsync();
+                .AsNoTracking()
+                .Include(l => l.Autor)
+                .ToListAsync();
         }
 
+        public async Task<IEnumerable<Livro>> BuscarPorTituloComAutorAsync(string titulo)
+        {
+            return await _dbContext.Livros
+                .AsNoTracking()
+                .Include(l => l.Autor)
+                .Where(l => l.Titulo != null && l.Titulo.Contains(titulo))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Livro>> BuscarPorGeneroComAutorAsync(EGenero genero)
+        {
+            return await _dbContext.Livros
+                .AsNoTracking()
+                .Include(l => l.Autor)
+                .Where(l => l.Genero == genero)
+                .ToListAsync();
+        }
     }
 }
