07bba7d54563c40adbd0d6a4686715ceec3f05db
Matheus <151960068+Namanosbad@users.noreply.github.com>
2026-02-26 17:05:17 -0300

    Refatora consulta de livros para incluir autor no data layer

--- Ohara.API.Domain/Interfaces/ILivroRepository.cs
+++ Ohara.API.Domain/Interfaces/ILivroRepository.cs
@@ -1,10 +1,13 @@
 ﻿using Ohara.API.Domain.Entities;
+using Ohara.API.Shared.Enums;
 
 namespace Ohara.API.Domain.Interfaces
 {
     public interface ILivroRepository
     {
-        Task<IEnumerable<Livro>> LivroPorGenero(string genero);
-        Task<IEnumerable<Livro>> BuscarPorTituloAsync(string titulo);
+        Task<Livro?> ObterPorIdComAutorAsync(Guid id);
+        Task<IEnumerable<Livro>> ObterTodosComAutorAsync();
+        Task<IEnumerable<Livro>> BuscarPorTituloComAutorAsync(string titulo);
+        Task<IEnumerable<Livro>> BuscarPorGeneroComAutorAsync(EGenero genero);
     }
 }
